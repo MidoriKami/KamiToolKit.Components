bdad2772fae7535dfbe13b5a7ee5eac80ada7e19
MidoriKami <mi_kami@outlook.com>
2026-06-07 21:40:06 -0700

    Add Gearset Search

---
+++ ListNodes/GearsetListItemNode.cs
@@ -0,0 +1,110 @@
+﻿using System.Numerics;
+using Dalamud.Game.Text;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes;
+using KamiToolKit.Interfaces;
+using KamiToolKit.Nodes;
+
+namespace KamiToolKit.Components.ListNodes;
+
+/// <summary>
+/// Implementation of <see cref="ListItemNode{T}"/> for use with <see cref="Search.GearsetSearchAddon"/>
+/// </summary>
+public class GearsetListItemNode : ListItemWithFocusNav<RaptureGearsetModule.GearsetEntry>, IListItemNode {
+
+    /// <inheritdoc/>
+    public static float ItemHeight => 48.0f;
+
+    /// <summary>
+    /// Gets the icon node used to display this item.
+    /// </summary>
+    protected IconImageNode ItemIconImageNode { get; }
+
+    /// <summary>
+    /// Gets the text node used to display the items name.
+    /// </summary>
+    protected TextNode GearsetNameTextNode { get; }
+
+    /// <summary>
+    /// Gets the text node used to display the items ItemUiCategory.
+    /// </summary>
+    protected TextNode GearsetItemLevelTextNode { get; }
+
+    /// <summary>
+    /// Gets the text node used to display the items ID.
+    /// </summary>
+    protected TextNode GearsetIndexTextNode { get; }
+
+    /// <inheritdoc/>
+    protected override void SetNodeData(RaptureGearsetModule.GearsetEntry itemData) {
+        if (!itemData.Flags.HasFlag(RaptureGearsetModule.GearsetFlag.Exists)) {
+            GearsetNameTextNode.IsVisible = true;
+            GearsetNameTextNode.String = "Invalid Gearset Id";
+
+            ItemIconImageNode.IsVisible = false;
+            GearsetItemLevelTextNode.IsVisible = false;
+            GearsetIndexTextNode.IsVisible = false;
+            return;
+        }
+
+        ItemIconImageNode.IconId = (uint) (itemData.ClassJob + 62000);
+        ItemIconImageNode.IsVisible = true;
+
+        GearsetNameTextNode.String = itemData.NameString;
+        GearsetNameTextNode.IsVisible = true;
+
+        GearsetItemLevelTextNode.String = $"{SeIconChar.ItemLevel.ToIconString()}{itemData.ItemLevel}";
+        GearsetItemLevelTextNode.IsVisible = true;
+
+        GearsetIndexTextNode.String = $"{itemData.Id + 1}";
+        GearsetIndexTextNode.IsVisible = true;
+    }
+
+    public GearsetListItemNode() {
+        ItemIconImageNode = new IconImageNode {
+            FitTexture = true,
+        };
+        ItemIconImageNode.AttachNode(this);
+
+        GearsetNameTextNode = new TextNode {
+            TextFlags = TextFlags.Ellipsis | TextFlags.Emboss,
+            FontSize = 14,
+            AlignmentType = AlignmentType.BottomLeft,
+        };
+        GearsetNameTextNode.AttachNode(this);
+
+        GearsetItemLevelTextNode = new TextNode {
+            TextFlags = TextFlags.Ellipsis | TextFlags.Emboss,
+            FontSize = 12,
+            AlignmentType = AlignmentType.TopLeft,
+            TextColor = ColorHelper.GetColor(3),
+            TextOutlineColor = ColorHelper.GetColor(7),
+        };
+        GearsetItemLevelTextNode.AttachNode(this);
+
+        GearsetIndexTextNode = new TextNode {
+            TextFlags = TextFlags.Emboss,
+            FontSize = 10,
+            AlignmentType = AlignmentType.BottomRight,
+            TextColor = ColorHelper.GetColor(3),
+        };
+        GearsetIndexTextNode.AttachNode(this);
+    }
+
+    protected override void OnSizeChanged() {
+        base.OnSizeChanged();
+
+        ItemIconImageNode.Size = new Vector2(Height - 4.0f, Height - 4.0f);
+        ItemIconImageNode.Position = new Vector2(2.0f, 2.0f);
+
+        GearsetIndexTextNode.Size = new Vector2(48.0f, Height / 2.0f);
+        GearsetIndexTextNode.Position = new Vector2(Width - GearsetIndexTextNode.Width, 0.0f);
+
+        GearsetNameTextNode.Size = new Vector2(Width - ItemIconImageNode.Width - GearsetIndexTextNode.Width - 8.0f, Height / 2.0f);
+        GearsetNameTextNode.Position = new Vector2(ItemIconImageNode.Bounds.Right + 4.0f, 0.0f);
+
+        GearsetItemLevelTextNode.Size = GearsetNameTextNode.Size;
+        GearsetItemLevelTextNode.Position = GearsetNameTextNode.Position with { Y = Height / 2.0f };
+    }
+}
---
+++ Search/GearsetSearchAddon.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using KamiToolKit.Components.ListNodes;
+using Lumina.Text.ReadOnly;
+
+namespace KamiToolKit.Components.Search;
+
+/// <summary>
+/// Implementation of <see cref="AbstractSearchAddon{T,TU}"/> for use with Gearsets.
+/// </summary>
+public class GearsetSearchAddon : AbstractSearchAddon<RaptureGearsetModule.GearsetEntry, GearsetListItemNode> {
+    protected override void OnSearchInputReceived(ReadOnlySeString searchString) {
+        var searchRegex = new Regex(searchString.ToString(),  RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+
+        ResultsListNode?.OptionsList = OptionsList.Where(option => searchRegex.IsMatch(option.NameString)).ToList();
+
+        base.OnSearchInputReceived(searchString);
+    }
+}
