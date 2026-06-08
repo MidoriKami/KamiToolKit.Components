d2aa640f8a386c99e89c1ea37f11e08d5dafd9ba
MidoriKami <mi_kami@outlook.com>
2026-06-08 13:27:09 -0700

    Populate gearsets by default for GearsetSearchAddon

--- Search/GearsetSearchAddon.cs
+++ Search/GearsetSearchAddon.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Components.ListItemNodes;
 using Lumina.Text.ReadOnly;
 
@@ -8,8 +10,20 @@ namespace KamiToolKit.Components.Search;
 
 /// <summary>
 /// Implementation of <see cref="AbstractSearchAddon{T,TU}"/> for use with Gearsets.
+/// By default, will populate <see cref="AbstractSearchAddon{T,TU}.OptionsList"/> with all available gearsets on open.
 /// </summary>
 public class GearsetSearchAddon : AbstractSearchAddon<RaptureGearsetModule.GearsetEntry, GearsetListItemNode> {
+
+    protected override unsafe void OnSetup(AtkUnitBase* addon, Span<AtkValue> atkValueSpan) {
+        OptionsList = RaptureGearsetModule.Instance()->Entries
+            .ToArray()
+            .Where(entry => entry.Flags.HasFlag(RaptureGearsetModule.GearsetFlag.Exists))
+            .OrderBy(entry => entry.Id)
+            .ToList();
+
+        base.OnSetup(addon, atkValueSpan);
+    }
+
     protected override void OnSearchInputReceived(ReadOnlySeString searchString) {
         var searchRegex = new Regex(searchString.ToString(),  RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
 
